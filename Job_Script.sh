985def71515273e64f07285ede17d0b6717700e8
Dilani Rajapakse <dr27977@uga.edu>
2025-04-01 20:17:20 -0400

    job script added

---
+++ Job_Script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=trim		                    # Job name
+#SBATCH --partition=batch		                        # Partition (queue) name
+#SBATCH --ntasks=1		                            # Single task job
+#SBATCH --cpus-per-task=6		                        # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=40gb			                            # Total memory for job
+#SBATCH --time=4:00:00  		                        # Time limit hrs:min:sec
+#SBATCH --output=/work/dr27977/H3K9me3ZF/scratch/log.%j			# Location of standard output and error log files 
+#SBATCH --mail-user=dr27977@uga.edu                    # Where to send mail 
+#SBATCH --mail-type=BEGIN,END,FAIL,ALL                            # Mail events (BEGIN, END, FAIL, ALL)
+
+OUTDIR="/work/dr27977/H3K9me3ZF/scratch/" 
+#if output directory doesn't exist, create it
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
