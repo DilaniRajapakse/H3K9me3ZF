d9c6b69bf1dfa37bfff90194fd41c21cce8418d0
Dilani Rajapakse <dr27977@uga.edu>
2025-04-09 20:21:30 -0400

    sorting e.coli aligned?

--- Job_Script.sh
+++ Job_Script.sh
@@ -60,56 +60,66 @@ ml STAR
 #done
 
  
-for file in $OUTDIR/trimmed/*_val_*.fq.gz;
+#for file in $OUTDIR/trimmed/*_val_*.fq.gz;
+#do
+#if [[ $prefix ]]; then
+        #base=$(basename ${first} _R1_val_1.fq.gz)
+        #STAR --runThreadN 20 --genomeDir $OUTDIR/ecoli_genome --outFileNamePrefix $OUTDIR/bams2/"$base"_ecoli \
+        #--readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
+        #--outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+
+        #STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams2/"$base"_ecoli \
+        #--readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
+        #--outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+
+        #STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams2/"$base"_ecoli \
+        #--readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
+        #--outMultimapperOrder Random --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+
+        #STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams2/"$base"_ecoli \
+        #--readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
+        #--outSAMprimaryFlag AllBestScore --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+        #prefix=
+        #else
+          #first=$file
+          #prefix=${file%%_*}
+       #fi
+       #done
+#Begin 4.9.25
+rm $OUTDIR/bams2/"$base"*SJ.out.tab
+
+ if [ -d "$OUTDIR/bams2/logs" ]
+ then
+     mv $OUTDIR/bams2/*Log* $OUTDIR/bams2/logs
+ else
+   mkdir $OUTDIR/bams2/logs
+   mv $OUTDIR/bams2/*Log* $OUTDIR/bams2/logs
+ fi
+
+module load SAMtools/1.18-GCC-12.3.0 
+
+for file in $OUTDIR/bams2/"$base"*ecoliAligned.sortedByCoord.out.bam
 do
-if [[ $prefix ]]; then
-        base=$(basename ${first} _R1_val_1.fq.gz)
-        STAR --runThreadN 20 --genomeDir $OUTDIR/ecoli_genome --outFileNamePrefix $OUTDIR/bams2/"$base"_ecoli \
-        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
-        --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+  base=$(basename ${file} ecoliAligned.sortedByCoord.out.bam)
+  samtools view -bq1 $file | samtools sort - > $OUTDIR/bams2/"$base"_ecoli_q1.bam
+done
 
-        STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams2/"$base"_ecoli \
-        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
-        --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
-
-        STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams2/"$base"_ecoli \
-        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
-        --outMultimapperOrder Random --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
-
-        STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams2/"$base"_ecoli \
-        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
-        --outSAMprimaryFlag AllBestScore --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
-        prefix=
-        else
-          first=$file
-          prefix=${file%%_*}
-       fi
-       done
-
-#module load SAMtools/1.18-GCC-12.3.0 
-
-#for file in $OUTDIR/bams/"$base"*ecoliAligned.sortedByCoord.out.bam
-#do
-  #base=$(basename ${file} ecoliAligned.sortedByCoord.out.bam)
-  #samtools view -bq1 $file | samtools sort - > $OUTDIR/bams/"$base"_ecoli_q1.bam
-#done
-
-#for infile in $OUTDIR/bams/"$base"*_ecoli_q1.bam
-#do
- #base=$(basename ${infile} _ecoli_q1.bam)
- #echo "$base total aligned reads -" >> $OUTDIR/bams/bam_stats.txt
- #samtools view -@ 24 -F 0x4 $OUTDIR/bams/"$base"ecoliAligned.sortedByCoord.out.bam | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams/bam_stats.txt
- #echo "  $base total aligned reads (unique mappers) -" >> $OUTDIR/bams/bam_stats.txt
- #samtools view -@ 24 -F 0x4 $OUTDIR/bams/"$base"ecoliAligned.sortedByCoord.out.bam | grep "NH:i:1" | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams/bam_stats.txt
- #echo "  $base total aligned reads (multi mappers) -" >> $OUTDIR/bams/bam_stats.txt
- #samtools view -@ 24 -F 0x4 $OUTDIR/bams/"$base"ecoliAligned.sortedByCoord.out.bam | grep -v "NH:i:1" | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams/bam_stats.txt
- #echo "$base q1 aligned reads -" >> $OUTDIR/bams/bam_stats.txt
- #samtools view -@ 24 -F 0x4 $infile | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams/bam_stats.txt
- #echo "  $base q1 aligned reads (unique mappers) -" >> $OUTDIR/bams/bam_stats.txt
- #samtools view -@ 24 -F 0x4 $infile | grep "NH:i:1" | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams/bam_stats.txt
- #echo "  $base q1 aligned reads (multi mappers) -" >> $OUTDIR/bams/bam_stats.txt
- #samtools view -@ 24 -F 0x4 $infile | grep -v "NH:i:1" | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams/bam_stats.txt
- #done
+for infile in $OUTDIR/bams2/"$base"*_ecoli_q1.bam
+do
+ base=$(basename ${infile} _ecoli_q1.bam)
+ echo "$base total aligned reads -" >> $OUTDIR/bams2/bam_stats.txt
+ samtools view -@ 24 -F 0x4 $OUTDIR/bams2/"$base"ecoliAligned.sortedByCoord.out.bam | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams2/bam_stats.txt
+ echo "  $base total aligned reads (unique mappers) -" >> $OUTDIR/bams2/bam_stats.txt
+ samtools view -@ 24 -F 0x4 $OUTDIR/bams2/"$base"ecoliAligned.sortedByCoord.out.bam | grep "NH:i:1" | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams2/bam_stats.txt
+ echo "  $base total aligned reads (multi mappers) -" >> $OUTDIR/bams2/bam_stats.txt
+ samtools view -@ 24 -F 0x4 $OUTDIR/bams2/"$base"ecoliAligned.sortedByCoord.out.bam | grep -v "NH:i:1" | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams2/bam_stats.txt
+ echo "$base q1 aligned reads -" >> $OUTDIR/bams2/bam_stats.txt
+ samtools view -@ 24 -F 0x4 $infile | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams2/bam_stats.txt
+ echo "  $base q1 aligned reads (unique mappers) -" >> $OUTDIR/bams2/bam_stats.txt
+ samtools view -@ 24 -F 0x4 $infile | grep "NH:i:1" | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams2/bam_stats.txt
+ echo "  $base q1 aligned reads (multi mappers) -" >> $OUTDIR/bams2/bam_stats.txt
+ samtools view -@ 24 -F 0x4 $infile | grep -v "NH:i:1" | cut -f 1 | sort | uniq | wc -l >> $OUTDIR/bams2/bam_stats.txt
+ done
 
 ####Remove PCR duplicates
 #ml picard/3.2.0-Java-17
