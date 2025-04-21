653c6319df6d03751f9ea64d1af62bc28eca1bb1
Dilani Rajapakse <dr27977@uga.edu>
2025-04-21 14:09:29 -0400

    re running trim and star and aligning to ecoli genome with all samples

--- CorrectScript.sh
+++ CorrectScript.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1	                                # Single task job
 #SBATCH --cpus-per-task=24		                            # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=120gb			                                # Total memory for job
-#SBATCH --time=50:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --time=72:00:00  		                            # Time limit hrs:min:sec
 #SBATCH --output=/scratch/dr27977/log.%j		    # Location of standard output and error log files (replace cbergman with your myid)
 #SBATCH --mail-user=dr27977@uga.edu                    # Where to send mail (replace cbergman with your myid)
 #SBATCH --mail-type=ALL                            # Mail events (BEGIN, END, FAIL, ALL)
@@ -25,49 +25,49 @@ fi
 
 BASEDIR="/scratch/dr27977/H3K9me3_Zebrafish/CUTnRUN_published"
 
-#ml STAR
-#for file in $OUTDIR/*_R*.fastq.gz;
-#do
-# if [[ $prefix ]]; then
-#        base=$(basename ${first} _R1.fastq.gz)
-#         sh /home/dr27977/H3K9me3ZF/PE_trim_and_star.sh -o $OUTDIR -n $base -m one $first $file
-#         prefix=
-#     else
-#         first=$file
-#         prefix=${file%%_*}
-#     fi
-# done
+ml STAR
+for file in $OUTDIR/*_R*.fastq.gz;
+do
+ if [[ $prefix ]]; then
+        base=$(basename ${first} _R1.fastq.gz)
+         sh /home/dr27977/H3K9me3ZF/PE_trim_and_star.sh -o $OUTDIR -n $base -m one $first $file
+         prefix=
+     else
+         first=$file
+         prefix=${file%%_*}
+     fi
+ done
 
  ##aligning to ecoli genome
-# curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c > $OUTDIR/ecoli_refseq.fa
+ curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz | gunzip -c > $OUTDIR/ecoli_refseq.fa
  # note here that STAR suggests SAindex = 10 but that makes the alignment FAIL, do 8 instead
-# STAR --runThreadN 20 --genomeSAindexNbases 8 --runMode genomeGenerate --genomeDir $OUTDIR/ecoli_genome --genomeFastaFiles $OUTDIR/ecoli_refseq.fa
+ STAR --runThreadN 20 --genomeSAindexNbases 8 --runMode genomeGenerate --genomeDir $OUTDIR/ecoli_genome --genomeFastaFiles $OUTDIR/ecoli_refseq.fa
 
-# for file in $OUTDIR/trimmed2/*_val_*.fq.gz;
-# do
-#   if [[ $prefix ]]; then
-#       base=$(basename ${first} _R1_val_1.fq.gz)
-#       STAR --runThreadN 20 --genomeDir $OUTDIR/ecoli_genome --outFileNamePrefix $OUTDIR/bams/"$base"_ecoli \
-#         --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
-#         --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+ for file in $OUTDIR/trimmed2/*_val_*.fq.gz;
+ do
+   if [[ $prefix ]]; then
+       base=$(basename ${first} _R1_val_1.fq.gz)
+       STAR --runThreadN 20 --genomeDir $OUTDIR/ecoli_genome --outFileNamePrefix $OUTDIR/bams/"$base"_ecoli \
+         --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
+         --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
  
-#       STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams/"$base"_ecoli \
-#        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
-#        --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+       STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams/"$base"_ecoli \
+        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
+        --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
 
-#       STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams/"$base"_ecoli \
-#        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
-#        --outMultimapperOrder Random --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+       STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams/"$base"_ecoli \
+        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
+        --outMultimapperOrder Random --outSAMmultNmax 1 --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
 
-#       STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams/"$base"_ecoli \
-#        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
-#        --outSAMprimaryFlag AllBestScore --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
-#         prefix=
-#     else
-#         first=$file
-#         prefix=${file%%_*}
-#     fi
-# done
+       STAR --runThreadN 20 --genomeDir $OUTDIR/genome --outFileNamePrefix $OUTDIR/bams/"$base"_ecoli \
+        --readFilesCommand zcat --readFilesIn "$first" "$file" --outSAMtype BAM SortedByCoordinate \
+        --outSAMprimaryFlag AllBestScore --alignEndsType EndToEnd --alignIntronMax 1 --alignMatesGapMax 2000
+         prefix=
+     else
+         first=$file
+         prefix=${file%%_*}
+     fi
+ done
 
 # rm $OUTDIR/bams/${base}*SJ.out.tab
 
