ad687f09ddf2421c795874d7e585facd8c53c104
Dilani Rajapakse <dr27977@uga.edu>
2025-05-27 09:22:39 -0400

    get peaks

--- NewGenomeScript.sh
+++ NewGenomeScript.sh
@@ -235,151 +235,210 @@ BASEDIR="/scratch/dr27977/H3K9me3_Zebrafish/NewGenome"
 #  bedtools intersect -a $infile -b $BASEDIR/peaks/blacklist.bed -v > $BASEDIR/peaks/"$base"_final.bed
 #done
 
-module load ucsc
-mkdir $BASEDIR/bws
+#module load ucsc
+#mkdir $BASEDIR/bws
 
-for infile in $BASEDIR/bdgrphs/*norm.bga
-do
-  base=$(basename ${infile} .norm.bga)
-  bedSort $infile $infile
-  bedGraphToBigWig $infile $BASEDIR/genome/chrNameLength.txt $BASEDIR/bws/$base.bw
-done
+#for infile in $BASEDIR/bdgrphs/*norm.bga
+#do
+#  base=$(basename ${infile} .norm.bga)
+#  bedSort $infile $infile
+#  bedGraphToBigWig $infile $BASEDIR/genome/chrNameLength.txt $BASEDIR/bws/$base.bw
+#done
 
 ###lets do some broad comparisons to see what our data looks like before moving on
-module load deepTools 
+#module load deepTools 
 
-multiBigwigSummary bins -b $BASEDIR/bws/*[1-3].bw $BASEDIR/bws/*IgG*.bw -o $BASEDIR/bwreps_summ.npz -p 24
-plotCorrelation -in $BASEDIR/bwreps_summ.npz -c spearman -p heatmap -o $BASEDIR/timecourse_bwreps_summ_heatmap.pdf
-plotPCA -in $BASEDIR/bwreps_summ.npz -o $BASEDIR/timecourse_bwreps_summ_PCA.pdf
+#multiBigwigSummary bins -b $BASEDIR/bws/*[1-3].bw $BASEDIR/bws/*IgG*.bw -o $BASEDIR/bwreps_summ.npz -p 24
+#plotCorrelation -in $BASEDIR/bwreps_summ.npz -c spearman -p heatmap -o $BASEDIR/timecourse_bwreps_summ_heatmap.pdf
+#plotPCA -in $BASEDIR/bwreps_summ.npz -o $BASEDIR/timecourse_bwreps_summ_PCA.pdf
 
 ## Making bws of the mean of the replicates for profile plots
-module load deepTools
-mkdir $OUTDIR/bws/bwscomp/
-bigwigCompare \
-  -b1 $OUTDIR/bws/2hpf_K9_1.bw \
-  -b2 $OUTDIR/bws/2hpf_K9_2.bw \
-  --operation add \
-  --scaleFactors 0.5:0.5 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/2hpf_K9_AVG.bw
+#module load deepTools
+#mkdir $OUTDIR/bws/bwscomp/
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/2hpf_K9_1.bw \
+#  -b2 $OUTDIR/bws/2hpf_K9_2.bw \
+#  --operation add \
+#  --scaleFactors 0.5:0.5 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/2hpf_K9_AVG.bw
 
  ##Step 1: Combine replicate 1 and 2 (each scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/K9abcam_2.5hpf_1.bw \
-  -b2 $OUTDIR/bws/K9abcam_2.5hpf_2.bw \
-  --operation add \
-  --scaleFactors 0.333:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/K9abcam_2.5hpf_1plus2.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/K9abcam_2.5hpf_1.bw \
+#  -b2 $OUTDIR/bws/K9abcam_2.5hpf_2.bw \
+#  --operation add \
+#  --scaleFactors 0.333:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/K9abcam_2.5hpf_1plus2.bw
 
 ## Step 2: Add replicate 3 to the combined 1+2 (again scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/bwscomp/K9abcam_2.5hpf_1plus2.bw \
-  -b2 $OUTDIR/bws/K9abcam_2.5hpf_3.bw \
-  --operation add \
-  --scaleFactors 1:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/K9abcam_2.5hpf_AVG.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/bwscomp/K9abcam_2.5hpf_1plus2.bw \
+#  -b2 $OUTDIR/bws/K9abcam_2.5hpf_3.bw \
+#  --operation add \
+#  --scaleFactors 1:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/K9abcam_2.5hpf_AVG.bw
 
 ## Step 1: Combine replicates 1 and 2, each scaled to 1/3
-bigwigCompare \
-  -b1 $OUTDIR/bws/3hpf_K9_1.bw \
-  -b2 $OUTDIR/bws/3hpf_K9_2.bw \
-  --operation add \
-  --scaleFactors 0.333:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/3hpf_K9_1plus2.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/3hpf_K9_1.bw \
+#  -b2 $OUTDIR/bws/3hpf_K9_2.bw \
+#  --operation add \
+#  --scaleFactors 0.333:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/3hpf_K9_1plus2.bw
 
 ## Step 2: Add replicate 3, also scaled to 1/3
-bigwigCompare \
-  -b1 $OUTDIR/bws/bwscomp/3hpf_K9_1plus2.bw \
-  -b2 $OUTDIR/bws/3hpf_K9_3.bw \
-  --operation add \
-  --scaleFactors 1:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/3hpf_K9_AVG.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/bwscomp/3hpf_K9_1plus2.bw \
+#  -b2 $OUTDIR/bws/3hpf_K9_3.bw \
+#  --operation add \
+#  --scaleFactors 1:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/3hpf_K9_AVG.bw
 
 ## Step 1: Combine replicates 1 and 2 (each scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/3.5hpf_K9_1.bw \
-  -b2 $OUTDIR/bws/3.5hpf_K9_2.bw \
-  --operation add \
-  --scaleFactors 0.333:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/3.5hpf_K9_1plus2.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/3.5hpf_K9_1.bw \
+#  -b2 $OUTDIR/bws/3.5hpf_K9_2.bw \
+#  --operation add \
+#  --scaleFactors 0.333:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/3.5hpf_K9_1plus2.bw
 
 ## Step 2: Add replicate 3 (also scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/bwscomp/3.5hpf_K9_1plus2.bw \
-  -b2 $OUTDIR/bws/3.5hpf_K9_3.bw \
-  --operation add \
-  --scaleFactors 1:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/3.5hpf_K9_AVG.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/bwscomp/3.5hpf_K9_1plus2.bw \
+#  -b2 $OUTDIR/bws/3.5hpf_K9_3.bw \
+#  --operation add \
+#  --scaleFactors 1:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/3.5hpf_K9_AVG.bw
 
 ## Step 1: Combine replicates 1 and 2 (each scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/4hpf_K9_1.bw \
-  -b2 $OUTDIR/bws/4hpf_K9_2.bw \
-  --operation add \
-  --scaleFactors 0.333:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/4hpf_K9_1plus2.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/4hpf_K9_1.bw \
+#  -b2 $OUTDIR/bws/4hpf_K9_2.bw \
+#  --operation add \
+#  --scaleFactors 0.333:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/4hpf_K9_1plus2.bw
 
 ## Step 2: Add replicate 3 (also scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/bwscomp/4hpf_K9_1plus2.bw \
-  -b2 $OUTDIR/bws/4hpf_K9_3.bw \
-  --operation add \
-  --scaleFactors 1:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/4hpf_K9_AVG.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/bwscomp/4hpf_K9_1plus2.bw \
+#  -b2 $OUTDIR/bws/4hpf_K9_3.bw \
+#  --operation add \
+#  --scaleFactors 1:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/4hpf_K9_AVG.bw
 
 ## Step 1: Combine replicates 1 and 2 (each scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/K9abcam_4.5hpf_1.bw \
-  -b2 $OUTDIR/bws/K9abcam_4.5hpf_2.bw \
-  --operation add \
-  --scaleFactors 0.333:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/K9abcam_4.5hpf_1plus2.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/K9abcam_4.5hpf_1.bw \
+#  -b2 $OUTDIR/bws/K9abcam_4.5hpf_2.bw \
+#  --operation add \
+#  --scaleFactors 0.333:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/K9abcam_4.5hpf_1plus2.bw
 
 ## Step 2: Add replicate 3 (also scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/bwscomp/K9abcam_4.5hpf_1plus2.bw \
-  -b2 $OUTDIR/bws/K9abcam_4.5hpf_3.bw \
-  --operation add \
-  --scaleFactors 1:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/K9abcam_4.5hpf_AVG.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/bwscomp/K9abcam_4.5hpf_1plus2.bw \
+#  -b2 $OUTDIR/bws/K9abcam_4.5hpf_3.bw \
+#  --operation add \
+#  --scaleFactors 1:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/K9abcam_4.5hpf_AVG.bw
 
 ##Step 1: Combine replicates 1 and 2 (each scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/K9abcam_24hpf_1.bw \
-  -b2 $OUTDIR/bws/K9abcam_24hpf_2.bw \
-  --operation add \
-  --scaleFactors 0.333:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/K9abcam_24hpf_1plus2.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/K9abcam_24hpf_1.bw \
+#  -b2 $OUTDIR/bws/K9abcam_24hpf_2.bw \
+#  --operation add \
+#  --scaleFactors 0.333:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/K9abcam_24hpf_1plus2.bw
 
 ## Step 2: Add replicate 3 (also scaled to 1/3)
-bigwigCompare \
-  -b1 $OUTDIR/bws/bwscomp/K9abcam_24hpf_1plus2.bw \
-  -b2 $OUTDIR/bws/K9abcam_24hpf_3.bw \
-  --operation add \
-  --scaleFactors 1:0.333 \
-  -bs 10 \
-  -p 20 \
-  -o $OUTDIR/bws/bwscomp/K9abcam_24hpf_AVG.bw
+#bigwigCompare \
+#  -b1 $OUTDIR/bws/bwscomp/K9abcam_24hpf_1plus2.bw \
+#  -b2 $OUTDIR/bws/K9abcam_24hpf_3.bw \
+#  --operation add \
+#  --scaleFactors 1:0.333 \
+#  -bs 10 \
+#  -p 20 \
+#  -o $OUTDIR/bws/bwscomp/K9abcam_24hpf_AVG.bw
+
+##Testing 5.27.25. Script that uses new genome annotation to find peaks
+module load HOMER
+module load SAMtools
+
+# Set base directory
+OUTDIR=/scratch/dr27977/H3K9me3_Zebrafish/NewGenome
+ANNOTDIR=$OUTDIR/annotation
+PEAKDIR=$OUTDIR/peaks
+ANNDIR=$PEAKDIR/ann
+
+mkdir -p $ANNOTDIR $ANNDIR
+
+# Download GTF and genome FASTA
+curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.gtf.gz | gunzip -c > $ANNOTDIR/GRCz12tu.gtf
+curl -s https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/049/306/965/GCF_049306965.1_GRCz12tu/GCF_049306965.1_GRCz12tu_genomic.fna.gz | gunzip -c > $ANNOTDIR/GRCz12tu.fa
+
+# Create genome size file
+samtools faidx $ANNOTDIR/GRCz12tu.fa
+cut -f1,2 $ANNOTDIR/GRCz12tu.fa.fai > $ANNOTDIR/GRCz12tu.genome
+
+# Annotate peaks using HOMER with GRCz12tu
+for infile in $PEAKDIR/*final.bed
+do
+  base=$(basename $infile final.bed)
+  annotatePeaks.pl $infile none \
+    -gtf $ANNOTDIR/GRCz12tu.gtf \
+    -genome $ANNOTDIR/GRCz12tu.genome \
+    > $ANNDIR/${base}.maskann.txt
+done
+
+# Filter peaks within ±1 kb of TSS
+for infile in $ANNDIR/*maskann.txt
+do
+  base=$(basename $infile .maskann.txt)
+  awk -F'\t' 'sqrt($10*$10) <= 1000' $infile > $ANNDIR/${base}.within1kb_TSS.txt
+done
+
+# Filter peaks within ±5 kb of TSS
+for infile in $ANNDIR/*maskann.txt
+do
+  base=$(basename $infile .maskann.txt)
+  awk -F'\t' 'sqrt($10*$10) <= 5000' $infile > $ANNDIR/${base}.within5kb_TSS.txt
+done
+
+# Classify by exon/intron/both
+for infile in $ANNDIR/*maskann.txt
+do
+  base=$(basename $infile .maskann.txt)
+
+  # Exon only
+  awk -F'\t' '$8 ~ /exon/ && $8 !~ /intron/' $infile > $ANNDIR/${base}.exon_only.txt
+
+  # Intron only
+  awk -F'\t' '$8 ~ /intron/ && $8 !~ /exon/' $infile > $ANNDIR/${base}.intron_only.txt
+
+  # Both exon and intron
+  awk -F'\t' '$8 ~ /exon/ && $8 ~ /intron/' $infile > $ANNDIR/${base}.exon_and_intron.txt
+done
