f1e031641d9a10ecb2ccb6d527157e3d3ffb627f
Dilani Rajapakse <dr27977@uga.edu>
2025-06-07 14:45:56 -0400

    Scripting properly

---
+++ scripts/create_annotations.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+##set expected directories for base script, input, and output
+BASE_DIR="/scratch/dr27977"
+INPUT_DIR="${BASE_DIR}/H3K9me3_Zebrafish/CUTnRUN_published/peaks"
+OUTPUT_DIR="${BASE_DIR}/OUTPUT/ANNOTATE"
+
+##create directories if they do not currently exist
+if [ ! -d $BASE_DIR ]
+then
+    mkdir -p $BASE_DIR
+fi
+cd $BASE_DIR
+
+if [ ! -d $INPUT_DIR ]
+then
+    mkdir -p $INPUT_DIR
+fi
+cd $INPUT_DIR
+
+if [ ! -d $OUTPUT_DIR ]
+then
+    mkdir -p $OUTPUT_DIR
+fi
+cd $OUTPUT_DIR
+
+##script start
+
+##load modules
+module load Homer/5.1-foss-2023a-R-4.3.2
+
+##get reference annotation
+curl -s ftp://ftp.ensembl.org/pub/release-98/gtf/danio_rerio/Danio_rerio.GRCz11.98.gtf.gz | gunzip -c > $OUTPUT_DIR/refann.gtf
+
+for infile in ${INPUT_DIR}/*final.bed
+  do
+    base=$( basename ${infile} _final.bed)
+    annotatePeaks.pl $infile danRer11 -gtf $OUTPUT_DIR/refann.gtf > $OUTPUT_DIR/${base}.maskann.txt
+  done
+
+##Filter peaks within 1kb of TSS
+for infile in $OUTPUT_DIR*maskann.txt
+  do
+    base=$(basename ${infile} .maskann.txt)
+    awk -F'\t' 'sqrt($10*$10) <=1000' $infile > $OUTPUT_DIR/${base}.1000bp_ann.txt
+  done
+
+## Filter peaks greater than 1Kb of TSS
+for infile in $OUTPUT_DIR*maskann.txt
+  do
+    base=$(basename ${infile} .maskann.txt)
+    awk -F'\t' 'sqrt($10*$10) >=1000' $infile | awk '{print $2 "\t" $3 "\t" $4 }' > $OUTPUT_DIR/${base}.MOREthan1000bp.bed
+  done
+
+##Filter peaks within 5kb of TSS
+for infile in $OUTPUT_DIR*maskann.txt
+  do
+    base=$(basename ${infile} .maskann.txt)
+    awk -F'\t' 'sqrt($10*$10) <=5000' $infile > $OUTPUT_DIR/${base}.5000bp_ann.txt
+  done
+
+## Filter peaks greater than 5Kb of TSS
+for infile in $OUTPUT_DIR*maskann.txt
+  do
+    base=$(basename ${infile} .maskann.txt)
+    awk -F'\t' 'sqrt($10*$10) >=5000' $infile | awk '{print $2 "\t" $3 "\t" $4 }' > $OUTPUT_DIR/${base}.MOREthan5000bp.bed
+  done
---
+++ scripts/create_tables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=CreateTables	                            # Job name
+#SBATCH --partition=batch		                            # Partition (queue) name
+#SBATCH --ntasks=1	                                        # Single task job
+#SBATCH --cpus-per-task=8		                            # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=80gb			                                # Total memory for job
+#SBATCH --time=72:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/scratch/dr27977/logs/output/log.%j.out
+#SBATCH --error=/scratch/dr27977/logs/error/log.%j.err		# Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --mail-user=dr27977@uga.edu                         # Where to send mail (replace cbergman with your myid)
+#SBATCH --mail-type=ALL                                     # Mail events (BEGIN, END, FAIL, ALL)
+
+SCRIPT_DIR="/home/dr27977/H3K9me3ZF/scripts"
+BASE_DIR="/scratch/dr27977/"
+
+sh ${SCRIPT_DIR}/create_annotations.sh
